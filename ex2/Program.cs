aba028f88816c6d75594b07ecd3ab69329b77307
Nik Krav <marshaque666@gmail.com>
2022-02-26 11:10:04 +0300

    задача 1

---
+++ ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число a: ");
+string strValue1 = Console.ReadLine();
+int a = int.Parse(strValue1);
+
+Console.Write("Введите число b: ");
+string strValue2 = Console.ReadLine();
+int b = int.Parse(strValue2);
+
+int квадрат = a * a;
+
+if (квадрат == b)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
